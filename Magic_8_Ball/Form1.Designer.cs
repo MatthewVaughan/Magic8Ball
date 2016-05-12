ec0e490eb5d2ebf66f564a7a29a28d65e90e3eb1
MatthewVaughan <mattman94@gmail.com>
2016-05-12 09:50:54 -0600

    Original Upload

---
+++ Magic_8_Ball/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Magic_8_Ball
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.img8Ball = new System.Windows.Forms.PictureBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.img8Ball)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // img8Ball
+            // 
+            this.img8Ball.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.img8Ball.Image = ((System.Drawing.Image)(resources.GetObject("img8Ball.Image")));
+            this.img8Ball.Location = new System.Drawing.Point(1, 27);
+            this.img8Ball.Name = "img8Ball";
+            this.img8Ball.Size = new System.Drawing.Size(791, 603);
+            this.img8Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img8Ball.TabIndex = 0;
+            this.img8Ball.TabStop = false;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblAnswer.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAnswer.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAnswer.Location = new System.Drawing.Point(312, 454);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(140, 56);
+            this.lblAnswer.TabIndex = 1;
+            this.lblAnswer.Text = "Shake to get an Answer";
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(280, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Click to Shake";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(792, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 630);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.img8Ball);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "Magic 8 Ball";
+            ((System.ComponentModel.ISupportInitialize)(this.img8Ball)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img8Ball;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Magic_8_Ball/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic_8_Ball
+{
+    public partial class FrmMain : Form
+    {
+        //Create random class
+        Random r = new Random();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Shake Button Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declare some strings
+            string
+                case1 = "Yes!",
+                case2 = "No!",
+                case3 = "Maybe",
+                case4 = "It is a Possibility",
+                case5 = "Not in a Million Years",
+                case6 = "It is very likely",
+                case7 = "Certainly",
+                case8 = "Try Again";
+
+            //Store number generated from the random generator
+            int numAnswer = r.Next(1, 9);
+
+            //Interpret each random number as a specific case and display appropriate string
+            switch (numAnswer)
+            {
+                case 1:
+                    lblAnswer.Text = case1;
+                    break;
+
+                case 2:
+                    lblAnswer.Text = case2;
+                    break;
+
+                case 3:
+                    lblAnswer.Text = case3;
+                    break;
+
+                case 4:
+                    lblAnswer.Text = case4;
+                    break;
+
+                case 5:
+                    lblAnswer.Text = case5;
+                    break;
+
+                case 6:
+                    lblAnswer.Text = case6;
+                    break;
+
+                case 7:
+                    lblAnswer.Text = case7;
+                    break;
+
+                case 8:
+                    lblAnswer.Text = case8;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// About Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Display message box showing programmers name and class
+            MessageBox.Show("Matthew Vaughan\nCS1400 Sec 003");
+        }
+
+        /// <summary>
+        /// Exit Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Close out of the program
+            this.Close();
+        }
+    }
+}
